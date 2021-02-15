e405595ffdb614f6e0ef85d37b596c755c688f57
Jose Pablo Loría León <jlorialeon@gmail.com>
2021-02-14 22:27:51 -0600

    add use state

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import react, { useState } from 'react'
+const inputElement = () => {
+
+    const [inputText, setInputText] = useState("");
+    const [historyList, setHistoryList] = useState([]);
+
+    return <div>
+        <input onChange={
+            (e) => {
+                setInputText(e.target.value);
+                setHistoryList([...historyList, e.target.value]);
+            }} placeholder="Enter some text" /><br />
+        {inputText}
+        <hr /><br />
+        <ul>
+            {historyList.map(rec=><div>{rec}</div>)}
+        </ul>
+    </div>
+}
+
+export default inputElement;
